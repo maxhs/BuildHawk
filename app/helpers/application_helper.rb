955507d8f56fe26e632f26f34bb9f7b8411ae6a0
Max <max@foodia.org>
2013-12-09 13:17:07 -0800

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 	def parse_time(time)
-		time.strftime("%b %e, %l:%M %p")
+		time.strftime("%b %e, %l:%M %p") if time
 	end
 	def parse_date(time)
-		time.strftime("%m/%d/%Y")
+		time.strftime("%m/%d/%Y") if time
 	end
 end
