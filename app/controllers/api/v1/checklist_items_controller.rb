7def54473cbe3b4a1f12029c32be044ad1f8fcb1
Max <max@foodia.org>
2013-12-11 01:53:08 -0800

    .

--- app/controllers/api/v1/checklist_items_controller.rb
+++ app/controllers/api/v1/checklist_items_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ChecklistItemsController < Api::V1::ApiController
 
     def update
-    	@item = ChecklistItem.find params[:id]
-    	@item.update_attributes params[:checklist_item]
+    	item = ChecklistItem.find params[:id]
+    	item.update_attributes params[:checklist_item]
     	respond_to do |format|
         	format.json { render_for_api :checklist, :json => item, :root => :checklist_item}
       	end
