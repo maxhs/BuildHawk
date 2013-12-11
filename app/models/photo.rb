5e88f4306aa21c41b2596b9ebecb20c07fd5d062
Max <max@foodia.org>
2013-12-11 02:45:47 -0800

    .

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -47,10 +47,10 @@ class Photo < ActiveRecord::Base
 	end
 
 	api_accessible :dashboard do |t|
-		#t.add :url1000
+		t.add :url1000
 		t.add :url500
 		t.add :url200
-		#t.add :url100
+		t.add :url100
 		t.add :source
 		#t.add :user
 		t.add :created_at
