445e442063eb9518b6bcf6211e287ca4383b5277
Max <max@foodia.org>
2013-12-14 19:42:03 -0800

    .

--- app/controllers/api/v1/punchlist_items_controller.rb
+++ app/controllers/api/v1/punchlist_items_controller.rb
@@ -1,18 +1,10 @@
 class Api::V1::PunchlistItemsController < Api::V1::ApiController
 
     def update
-    	@item = PunchlistItem.find params[:id]
-    	@item.update_attributes params[:punchlist_item]
+    	@punchlist_item = PunchlistItem.find params[:id]
+    	@punchlist_item.update_attributes params[:punchlist_item]
     	respond_to do |format|
-        	format.json { render_for_api :punchlist, :json => item, :root => :punchlist_item}
-      	end
-    end
-
-    def show
-    	project = Project.find params[:id]
-    	@checklist = project.checklist
-    	respond_to do |format|
-        	format.json { render_for_api :checklist, :json => @checklist, :root => :checklist}
+        	format.json { render_for_api :punchlist, :json => @punchlist_item, :root => :punchlist_item}
       	end
     end
 
