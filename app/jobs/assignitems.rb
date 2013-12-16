81288d2b8094ae712e2c098fa0a6294f10b91902
Max <max@foodia.org>
2013-12-16 01:46:58 -0800

    .

--- app/jobs/assignitems.rb
+++ app/jobs/assignitems.rb
@@ -4,6 +4,5 @@ module AssignItems
   		puts "assigning checklist items in the background"
   		checklist = Checklist.find checklist_id
     	checklist.checklist_items << checklist.categories.order('name').map(&:subcategories).flatten.map(&:checklist_items).flatten
-    	checklist.save!
   	end
 end
