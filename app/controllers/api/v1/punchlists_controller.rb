3231227df68f16de87309a47ec4c22df5981e409
Max <max@foodia.org>
2013-12-17 18:34:07 -0800

    .

--- app/controllers/api/v1/punchlists_controller.rb
+++ app/controllers/api/v1/punchlists_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::PunchlistsController < Api::V1::ApiController
     	if project.punchlists && project.punchlists.count > 0
     		@punchlist = project.punchlists.first
     	else 
-    		project.punchlists.create
+    		@punchlist = project.punchlists.create
     	end
     	respond_to do |format|
         	format.json { render_for_api :punchlist, :json => @punchlist, :root => :punchlist}
