d326b61655fd7ae909231f1377b22b886f0b3ec8
Max <max@foodia.org>
2013-12-18 09:08:26 -0800

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,10 @@ class User < ActiveRecord::Base
       password.present?
     end
 
+    def coworkers
+      company.users.map{|user| {:full_namy => user.full_name, :email => :user.email}}
+    end
+
   	acts_as_api
 
   	api_accessible :user do |t|
@@ -48,7 +52,7 @@ class User < ActiveRecord::Base
 	    t.add :email
 	    t.add :phone_number
       t.add :authentication_token
-      t.add :company
+      t.add :coworkers
   	end
 
   	api_accessible :feed do |t|
