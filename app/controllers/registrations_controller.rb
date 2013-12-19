b36e947c89d4ea2f2f56905f880af80d6d03df69
Max <max@foodia.org>
2013-12-18 22:41:11 -0800

    .

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,7 +10,9 @@ class RegistrationsController < Devise::RegistrationsController
     @company = Company.where(name: params[:user][:company][:name]).first_or_create!
     @company.projects.build unless @company.projects.count
     current_user.update_attribute :company_id, @company.id
-
+    if @company.users.first.id == current_user.id
+        current_user.update_attribute :admin, true
+      end
   end
 
   def update
