eb2d519126e98af644694e5b3e1c57f9b57e6720
Max <max@foodia.org>
2013-12-25 11:46:40 -0500

    .

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,2 +1,5 @@
 class Sub < ActiveRecord::Base
+	attr_accessible :name, :company_id, :company, :email, :phone_number
+  	belongs_to :company
+  	has_many :users
 end
--- db/migrate/20131221185622_add_more_photo_attributes.rb
+++ db/migrate/20131221185622_add_more_photo_attributes.rb
@@ -1,4 +1,5 @@
 class AddMorePhotoAttributes < ActiveRecord::Migration
   def change
+  	add_column :photos, :name, :string, :default => ""
   end
 end
