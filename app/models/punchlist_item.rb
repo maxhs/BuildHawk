9e52077a0a27cd0943c81b9ffd332f0d588b012b
Max <max@foodia.org>
2013-12-30 14:42:54 -0500

    .

--- app/models/punchlist_item.rb
+++ app/models/punchlist_item.rb
@@ -21,7 +21,7 @@ class PunchlistItem < ActiveRecord::Base
         if assignee && assignee.full_name
             unless assignee_name && assignee_name == assignee.full_name
                 truncated = truncate(body, length:15)
-                message = "\"#{truncated}\" has been assigned to you on #{project.name}"
+                message = "\"#{truncated}\" has been assigned to you on #{Project.find(project_id).name}"
                 Notification.create(
                     :message            => message,
                     :user_id            => assignee.id,
