7ae1c17c7cef1e2c98037d83de9ba42edbd10eb2
Max <max@foodia.org>
2013-12-30 12:37:19 -0500

    .

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,17 +5,26 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    # add custom create logic here
-    super
     @company = Company.where(name: params[:user][:company][:name]).first_or_create!
     @company.projects.build unless @company.projects.count
+    super
     current_user.update_attribute :company_id, @company.id
     if @company.users.first.id == current_user.id
-        current_user.update_attribute :admin, true
-      end
+      current_user.update_attribute :admin, true
+    end
   end
 
   def update
     super
+  end
+
+  private
+
+  def after_sign_up_path_for(resource)
+    if @company.users.count == 0
+      admin_index_path
+    else
+      projects_path
+    end
   end
 end 
--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['POSTMARK_SMTP_SERVER'],
   :user_name      => ENV['POSTMARK_API_KEY'],
   :password       => ENV['POSTMARK_API_KEY'],
-  :domain         => 'buildhawk.heroku.com',
+  :domain         => 'www.buildhawk.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
