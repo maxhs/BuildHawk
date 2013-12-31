708149477d4d43b10ba86e023edbb52e34853b9c
Max <max@foodia.org>
2013-12-31 15:31:25 -0500

    >

--- app/models/report_sub.rb
+++ app/models/report_sub.rb
@@ -2,4 +2,11 @@ class ReportSub < ActiveRecord::Base
 	attr_accessible :report_id, :sub_id
 	belongs_to :report
 	belongs_to :sub
+
+	acts_as_api
+
+  	api_accessible :report do |t|
+  		t.add :id
+  		t.add :user
+  	end
 end
--- app/models/report_user.rb
+++ app/models/report_user.rb
@@ -2,4 +2,11 @@ class ReportUser < ActiveRecord::Base
 	attr_accessible :report_id, :user_id
 	belongs_to :report
 	belongs_to :user
+
+	acts_as_api
+
+  	api_accessible :report do |t|
+  		t.add :id
+  		t.add :user
+  	end
 end
