8325d6a8a7fcda5e678c06a207e5a154b473fdeb
Max <max@foodia.org>
2014-01-01 09:42:02 -0500

    .

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ReportsController < Api::V1::ApiController
 
     def update
+        @current_user = User.find params[:user_id] 
     	report = Report.find params[:id]
         if params[:report][:users].present?
             users = params[:report][:users]
@@ -11,16 +12,29 @@ class Api::V1::ReportsController < Api::V1::ApiController
                     ru = report.report_users.where(:user_id => user.id).first_or_create
                     ru.update_attribute :count, u[:count]
                     puts "creating a new report user: #{user.full_name}"
-                else
-                    report_sub = report.report_subs.create :count => u[:count], :name => u[:name]
-                    puts "just created a new report sub: #{report_sub}" 
                 end
             end
             params[:report].delete(:users)
         end
+
+        if params[:report][:subs].present?
+            subs = params[:report][:subs]
+            subs.each do |s|
+                puts "s: #{s} and :#{s[:name]}"
+                sub = Sub.find_by name: s[:name]
+                if sub
+                    the_sub = report.report_subs.where(:sub_id => sub.id).first_or_create
+                    the_sub.update_attributes :count => u[:count], :company_id => @current_user.company.id
+                    puts "found a sub for report: #{the_sub.name}"
+                else 
+                    the_sub = report.report_subs.create :name => s[:name], :count => u[:count], :company_id => @current_user.company.id
+                    puts "creating a new report sub: #{the_sub.name}"
+                end
+            end
+            params[:report].delete(:subs)
+        end
+
     	report.update_attributes params[:report]
-        #project = report.project
-        #@reports = project.reports
     	respond_to do |format|
         	format.json { render_for_api :report, :json => report, :root => :report}
       	end
