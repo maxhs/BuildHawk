b015878a938b3bf1fb64d93e9725259590fe9797
Max <max@foodia.org>
2014-01-02 09:55:27 -0800

    .

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ReportsController < Api::V1::ApiController
 
     def update
-        @current_user = User.find params[:user_id] 
+        @current_user = User.find params[:author_id] 
     	report = Report.find params[:id]
         if params[:report][:users].present?
             users = params[:report][:users]
