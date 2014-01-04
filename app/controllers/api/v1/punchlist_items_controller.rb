89de98a806d810fc74f5deb0dc7615eec4da255e
Max <max@foodia.org>
2014-01-04 10:02:41 -0800

    .

--- app/controllers/api/v1/punchlist_items_controller.rb
+++ app/controllers/api/v1/punchlist_items_controller.rb
@@ -29,14 +29,22 @@ class Api::V1::PunchlistItemsController < Api::V1::ApiController
 
     def create
         @project = Project.find params[:project_id]
-        @punchlist_item = @project.punchlists.last.punchlist_items.create params[:punchlist_item]
+
         if params[:punchlist_item][:assignee].present? && params[:punchlist_item][:assignee].length > 0
-            user = User.where(:full_name => params[:punchlist_item][:assignee]).first
-            if user
-                @punchlist_item.update_attribute :assignee_id, user.id
-            else
-                @punchlist_item.update_attribute :assignee_name, params[:punchlist_item][:assignee]
-            end
+            assignee = [:punchlist_item][:assignee]
+            params[:punchlist_item].delete(:assignee)
+        end
+
+        @punchlist_item = @project.punchlists.last.punchlist_items.create params[:punchlist_item]
+
+        user = User.where(:full_name => assignee).first
+        if user
+            @punchlist_item.update_attribute :assignee_id, user.id
+            puts "assignee is a user: #{user.full_name}"
+        else
+            sub = Sub.where(:name => assignee).first_or_create
+            @punchlist_item.update_attribute :assignee_id, sub.id
+            puts "assignee is a sub: #{sub.name}"
         end
 
         if @punchlist_item.save
