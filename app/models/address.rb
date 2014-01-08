fe34e7437071b828b5eedcc2cdf8486a3df70c86
Max <max@foodia.org>
2014-01-07 16:03:54 -0800

    >

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,12 @@
 class Address < ActiveRecord::Base
-    attr_accessible :street1, :street2, :city, :zip, :country, :phone_number, :state
+    attr_accessible :street1, :street2, :city, :zip, :country, :phone_number, :state, :latitude, :longitude
   	belongs_to :user
   	belongs_to :company
   	belongs_to :project
 
+    reverse_geocoded_by :latitude, :longitude
+    geocoded_by :formatted_address
+    after_validation :geocode
 
     def formatted_address
       "#{street1}, #{city}, #{state} #{zip}"
@@ -19,6 +22,8 @@ class Address < ActiveRecord::Base
   		t.add :country
   		t.add :phone_number
       t.add :formatted_address
+      t.add :latitude
+      t.add :longitude
   	end
 
     api_accessible :user, :extend => :projects do |t|
