e110ea7fd4be2f7e07d486570644da72ec98b4ce
Max <max@foodia.org>
2014-01-10 13:30:54 -0800

    >

--- app/controllers/api/v1/punchlist_items_controller.rb
+++ app/controllers/api/v1/punchlist_items_controller.rb
@@ -32,10 +32,10 @@ class Api::V1::PunchlistItemsController < Api::V1::ApiController
         @project = Project.find params[:punchlist_item][:project_id]
         puts "hey now: #{params[:punchlist_item]}"
 
-        if params[:punchlist_item][:user_assignee].present? 
-            user_assignee = [:punchlist_item][:user_assignee]
+        if params[:punchlist_item]["user_assignee"].present? 
+            user_assignee = [:punchlist_item]["user_assignee"]
             puts "punchlist item has a user assignee: #{user_assignee}"
-            params[:punchlist_item].delete(:user_assignee)
+            params[:punchlist_item].delete("user_assigne")
         elsif params[:punchlist_item][:sub_assignee].present?
             sub_assignee = [:punchlist_item][:sub_assignee]
             puts "punchlist item has a user assignee: #{sub_assignee}"
