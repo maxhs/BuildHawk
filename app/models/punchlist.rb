46eeeb4338dc8554d9a7635b468a9f45bdebc1a5
Max <max@foodia.org>
2014-01-13 15:33:56 -0800

    .

--- app/models/punchlist.rb
+++ app/models/punchlist.rb
@@ -5,19 +5,19 @@ class Punchlist < ActiveRecord::Base
   	belongs_to :project
 
     def personnel
-        punchlist_items.map(&:sub_assignee).flatten + punchlist_items.map(&:assignee).flatten
+      (punchlist_items.map(&:sub_assignee).flatten + punchlist_items.map(&:assignee).flatten).uniq.compact
     end
 
 	acts_as_api
 
   	api_accessible :user do |t|
   		t.add :punchlist_items
-        t.add :personnel
+      t.add :personnel
   	end
 
   	api_accessible :projects do |t|
   		t.add :punchlist_items
-        t.add :personnel
+      t.add :personnel
   	end
 
     api_accessible :punchlist do |t|
