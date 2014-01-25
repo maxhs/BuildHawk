b17b18adbfa2c843abd9d44e5b55fdc72f92db6e
Max <max@foodia.org>
2014-01-25 15:48:54 -0800

    .

--- db/migrate/20140124021341_add_folder_to_documents.rb
+++ db/migrate/20140124021341_add_folder_to_documents.rb
@@ -1,8 +1,7 @@
 class AddFolderToDocuments < ActiveRecord::Migration
   def change
-  	remove_column :photos, :folder, :string
-  	#add_column :photos, :folder, :string, :default => ""
-  	#change_column :photos, :folder, :string, null: false
-  	#change_column :photos, :name, :string, :default => "", null: false
+  	add_column :photos, :folder, :string, :default => ""
+  	change_column :photos, :folder, :string, null: false
+  	change_column :photos, :name, :string, :default => "", null: false
   end
 end
