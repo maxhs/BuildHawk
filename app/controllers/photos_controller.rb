ca9e78bf42c26d55e732485da396c5e691150358
Max <max@foodia.org>
2014-02-05 10:42:34 -0800

    .

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -28,4 +28,18 @@ class PhotosController < ApplicationController
 			redirect_to document_photos_project_path(@project)
 		end
 	end
+
+	def update
+		@photo = Photo.find params[:id]
+		@photo.update_attributes params[:photo]
+		if @photo.save && request.xhr?
+			respond_to do |format|
+				format.js
+			end
+		elsif @photo.save 
+			render :json => {:success => true}
+		else 
+			render :json => {:success => false}
+		end
+	end
 end
