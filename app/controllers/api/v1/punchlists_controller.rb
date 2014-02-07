88f17c00e6adbd322c39bd0e70798fbd3e79f0b5
Max <max@foodia.org>
2014-02-07 10:29:12 -0800

    .

--- app/controllers/api/v1/punchlists_controller.rb
+++ app/controllers/api/v1/punchlists_controller.rb
@@ -3,12 +3,13 @@ class Api::V1::PunchlistsController < Api::V1::ApiController
     def show
     	project = Project.find params[:id]
     	if project.punchlists && project.punchlists.count > 0
-    		@punchlist = project.punchlists.first
+    		punchlist = project.punchlists.first
+            puts "punchlist items count from api: #{punchlist.punchlist_items.count}"
     	else 
-    		@punchlist = project.punchlists.create
+    		punchlist = project.punchlists.create
     	end
     	respond_to do |format|
-        	format.json { render_for_api :punchlist, :json => @punchlist, :root => :punchlist}
+        	format.json { render_for_api :punchlist, :json => punchlist, :root => :punchlist}
       	end
     end
 
