5c08ca5e83a00c3dde07c2bd6f58eb9bb51ed688
Max <max@foodia.org>
2014-02-22 14:00:53 -0800

    .

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,33 @@
 class ChargesController < ApplicationController
+	before_filter :authenticate_user!
+	def new
+		company = current_user.company
+	  	active_projects = company.projects.where(:active => true).count
+	  	@amount = active_projects * 1000 / 100
+	  	puts "the amount is: #{@amount}"
+	end
+
+	def create
+	  # Amount in cents
+	  company = current_user.company
+	  active_projects = company.projects.where(:active => true).flatten
+	  @amount = active_projects * 1000
+	  puts "the amount is: #{@amount}"
+	  customer = Stripe::Customer.create(
+	    :email => current_user.email,
+	    :card  => params[:stripeToken]
+	  )
+
+	  charge = Stripe::Charge.create(
+	    :customer    => customer.id,
+	    :amount      => @amount,
+	    :description => company.name,
+	    :currency    => 'usd'
+	  )
+
+	rescue Stripe::CardError => e
+	  flash[:error] = e.message
+	  redirect_to charges_path
+	end
+
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => ENV['PUBLISHABLE_KEY'] || "pk_test_23emORqBWZz8YhjkR2vsXjSm",
+  :secret_key      => ENV['SECRET_KEY'] || "sk_test_BUXFDUpWt1ZFZ3s6NBzqBOFG"
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
