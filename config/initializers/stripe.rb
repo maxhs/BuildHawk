c4bce57f9eaa4d922054635dcc5cebbd81c4780e
Max <max@foodia.org>
2014-02-22 22:13:32 -0800

    .

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,9 @@
 Rails.configuration.stripe = {
-  #:publishable_key => ENV['PUBLISHABLE_KEY'] || "pk_test_23emORqBWZz8YhjkR2vsXjSm",
-  #:secret_key      => ENV['SECRET_KEY'] || "sk_test_BUXFDUpWt1ZFZ3s6NBzqBOFG"
-  :publishable_key => ENV['PUBLISHABLE_KEY'] || "pk_test_8JBQJLxliyXgo5GCgT85PvMf",
-  :secret_key      => ENV['SECRET_KEY'] || "sk_test_Bp7u2F08RTln53YMKDYqm3mW"
+  :publishable_key => ENV['PUBLISHABLE_KEY'] || "pk_test_23emORqBWZz8YhjkR2vsXjSm",
+  :secret_key      => ENV['SECRET_KEY'] || "sk_test_BUXFDUpWt1ZFZ3s6NBzqBOFG"
+  ## LIVE
+  #:publishable_key => ENV['PUBLISHABLE_KEY'] || "pk_live_weQFPFprD1yqXFhy8RiqdRn0",
+  #:secret_key      => ENV['SECRET_KEY'] || "sk_live_873aFSibbbwVdttkIVScEhNc"
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
