65d21383fff1ab46577a1dd804246c04b70ddba9
Max <max@foodia.org>
2014-02-25 12:07:38 -0800

    .

--- app/models/punchlist_item.rb
+++ app/models/punchlist_item.rb
@@ -45,7 +45,7 @@ class PunchlistItem < ActiveRecord::Base
                 message = "#{punchlist.project.name} (Worklist) - \"#{truncated}\" was just completed"
             #end
             Notification.where(:message => message,:user_id => self.user_id, :punchlist_item_id => self.id, :notification_type => "Worklist").first_or_create
-        elsif user_id && user.notifications.where(:punchlist_item_id => id).count > 0
+        elsif user_id
             message = "#{punchlist.project.name} (Worklist) - \"#{truncated}\" has been modified"
             Notification.where(:message => message,:user_id => user_id, :punchlist_item_id => id,:notification_type => "Worklist").create
         elsif assignee
