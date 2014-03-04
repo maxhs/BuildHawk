fe508078a9933c1d5e7fe117460710e7047f5869
Max <max@foodia.org>
2014-03-04 11:05:25 -0800

    .

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,12 @@
 class UserMailer < ActionMailer::Base
-
-  def welcome(user)
-  	@user = user
-  	mail(
-      :subject => "Welcome to BuildHawk!",
-      :to      => user.email,
-      :from    => "support@buildhawk.com",
-      :tag     => 'Welcome'
-    )
-
-  end
+	layout "list_mailer"
+	def welcome(user)
+		@user = user
+		mail(
+	  		:subject => "Welcome to BuildHawk!",
+	  		:to      => user.email,
+	  		:from    => "support@buildhawk.com",
+	  		:tag     => 'Welcome'
+		)
+	end
 end
