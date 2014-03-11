5998bd092ad50770d1e95b7a29e88137b73d2398
Max <max@foodia.org>
2014-03-11 12:03:37 -0700

    .

--- app/models/project_group.rb
+++ app/models/project_group.rb
@@ -1,7 +1,19 @@
 class ProjectGroup < ActiveRecord::Base
-	attr_accessible :company_id, :name
+	attr_accessible :company_id, :name, :projects_count
 
 	belongs_to :company
 	has_many :projects
 
+	def group_projects
+		Hash[projects.map(&:name)]
+	end
+	
+	acts_as_api
+
+  	api_accessible :projects do |t|
+  		t.add :name
+  		t.add :id
+  		t.add :projects_count
+  		t.add :group_projects
+  	end
 end
