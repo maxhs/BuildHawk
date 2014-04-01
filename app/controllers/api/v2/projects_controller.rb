854b39609f455d67970fa21f5b8c2655dfeeaf66
Max <max@foodia.org>
2014-04-01 10:54:35 -0700

    .

--- app/controllers/api/v2/projects_controller.rb
+++ app/controllers/api/v2/projects_controller.rb
@@ -1,16 +1,18 @@
 class Api::V2::ProjectsController < Api::V2::ApiController
 
     def index
-    	@user = User.find params[:user_id]
-    	projects = @user.projects.where(:project_group_id => nil).order("name ASC")
-        groups = @user.projects.where("project_group_id IS NOT NULL").map(&:project_group_id).uniq
-        if groups && groups.count > 0 
+    	user = User.find params[:user_id]
+    	projects = user.projects.where(:project_group_id => nil).order("name ASC")
+        projects += Project.where(:core => true).flatten
+        
+        groups = user.projects.where("project_group_id IS NOT NULL").map(&:project_group_id).uniq
+        if groups
             groups.each do |g|
                 projects << ProjectGroup.find(g).projects.first
             end 
         end
         
-        if projects && projects.count > 0
+        if projects
         	respond_to do |format|
             	format.json { render_for_api :projects, :json => projects, :root => :projects}
           	end
