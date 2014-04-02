226be506f03ea2439d994549fe42bec6d300e3bb
Max <max@foodia.org>
2014-04-02 13:13:35 -0700

    .

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,2 +1,8 @@
 class SubcategoriesController < ApplicationController
+
+	def edit
+		@subcategory = Subcategory.find params[:id]
+		@project = Project.find params[:project_id] if params[:project_id]
+		@projects = @project.company.projects
+	end
 end
