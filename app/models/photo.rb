65e20079f8e0aee0af4513e66b1b4c381c57bda5
Max <max@foodia.org>
2014-04-01 22:55:12 -0700

    .

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -31,7 +31,7 @@ class Photo < ActiveRecord::Base
         text    :folder do
         	folder.name if folder
         end        
-        #integer :project_id
+        integer :project_id
         #integer :report_id
         #integer :checklist_item_id
         #integer :punchlist_item_id
