88c9d22d9fcba983d254f3670418beff9dcc0636
Max <max@foodia.org>
2014-04-07 23:45:33 -0700

    .

--- app/controllers/api/v2/projects_controller.rb
+++ app/controllers/api/v2/projects_controller.rb
@@ -4,7 +4,8 @@ class Api::V2::ProjectsController < Api::V2::ApiController
         #find_projects
         @user = User.find params[:user_id]
         @projects = @user.project_users.where(:archived => false, :project_group_id => nil, :core => false).map(&:project).compact 
-        @projects += @user.project_users.where(:archived => false, :core => true).map(&:project).compact 
+        #@projects += @user.project_users.where(:archived => false, :core => true).map(&:project).compact 
+        @projects += Project.where(:core => true) 
         groups = @user.project_users.where("project_group_id IS NOT NULL").map(&:project_group_id).uniq
         if groups
             groups.each do |g|
