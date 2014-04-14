3381b93b71f8c7d62bb065887ee5f19e14e66cb7
Max <max@foodia.org>
2014-04-13 22:17:54 -0700

    .

--- app/controllers/api/v2/subs_controller.rb
+++ app/controllers/api/v2/subs_controller.rb
@@ -2,8 +2,14 @@ class Api::V2::SubsController < Api::V2::ApiController
 	
 	def create
 		@project = Project.find params[:project_id]
-		sub = Sub.create params
-		sub.update_attributes params[:sub]
+		sub = @project.subs.where(:name => params[:name]).first_or_create
+		if sub.save
+			respond_to do |format|
+        		format.json { render_for_api :report, :json => sub, :root => :sub}
+      		end
+		else
+			render json: { failure: false }
+		end
 	end
 
 end
