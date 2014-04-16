4fc0fe7cca575485a3c28c00a7f8e685a0442f39
Max <max@foodia.org>
2014-04-16 08:26:53 -0700

    .

--- app/assets/javascripts/safety_topics.js
+++ app/assets/javascripts/safety_topics.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function readMoreLess() {
+	$('.read-more').click(function(){
+		var clicked = $(this);
+		var $parent = $(this).parent();
+		var $hidden = $parent.find($('.hiddenText'));
+	    if($hidden.css('display') === 'none') {
+	    	$parent.find('.ellipsis').replaceWith($hidden);
+	        $hidden.show(100).css('display','inline');
+	        clicked.text("Read Less");
+	    } else {
+	        clicked.text("Read More");
+	        $hidden.before('<span class="ellipsis"> ... </span>');
+	        $hidden.hide(100);
+	    }
+	});
+}
