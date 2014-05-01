6336c1e043c75bfd2a73eec3b370d3a8871412e9
Max <max@foodia.org>
2014-05-01 12:05:15 -0700

    .

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,6 @@
 class AdminController < ApplicationController
 	before_filter :authenticate_user!
 	before_filter :find_user
-	layout 'admin'	
 	require 'stripe'
 
 	def index
--- app/controllers/uber_admin_controller.rb
+++ app/controllers/uber_admin_controller.rb
@@ -1,6 +1,5 @@
 class UberAdminController < ApplicationController
 	before_filter :authenticate_user!
-	layout 'uber_admin'
 	
 	def index
 		@companies = Company.all
