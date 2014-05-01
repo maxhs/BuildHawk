feb5a6d11f138a81319a14944f3c7d5ad6c20414
Max <max@foodia.org>
2014-05-01 14:16:54 -0700

    .

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -185,8 +185,8 @@ class AdminController < ApplicationController
 	def create_template
 		list = Checklist.find params[:checklist_id]
 		@checklist = list.duplicate
+		@checklist.company_id = @company.id
 		@checklist.save
-		@checklist.update_attribute :company_id, params[:company_id]
 		
 		@checklists = @user.company.checklists.where(:core => true).flatten
 		if request.xhr?
--- app/jobs/solr_update.rb
+++ app/jobs/solr_update.rb
@@ -2,7 +2,7 @@ class SolrUpdate
   @queue = :solr
   
   def self.perform(classname, id)
-  	puts "performing solr update for: #{classname - id}"
+  	puts "performing solr update for: #{classname id}"
     classname.constantize.find(id).solr_index
   end  
 end
