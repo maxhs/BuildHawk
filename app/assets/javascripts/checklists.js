44da57660903b3bbfff7f0582c76ba137ed9baba
Max <max@foodia.org>
2014-05-05 16:42:19 -0700

    .

--- app/assets/javascripts/checklists.js
+++ app/assets/javascripts/checklists.js
@@ -24,18 +24,4 @@ function checklistItem(){
     $('#clear-critical-date').click(function(){
     	$('#dp').val('');
     });
-	$('#export-checklist-item').on('click',function(){
-		if (!$('#export-checklist-modal').length > 0){
-			$('#item-container').prepend('<%= j render partial:"checklist_item_export", locals: {:item => item} %>');
-		}
-
-		$('body').append('<div class="modal-backdrop in"></div>');
-		$('#export-checklist-modal').animate({"left":"25%",'opacity':'1'},300);
-		$('#cancel-checklist-modal,.modal-backdrop.in').on('click',function(){
-			$('#export-checklist-modal').animate({"left":"100%",'opacity':'0'},300);
-			$('.modal-backdrop.in').fadeOut(300,function(){
-				$(this).remove();
-			});
-		});
-	});
 }
