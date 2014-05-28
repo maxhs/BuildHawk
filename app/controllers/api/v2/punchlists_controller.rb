ebe51c7919b14c8fe1713d730f5cd8ed58d8b9c9
Max <max@foodia.org>
2014-05-28 15:45:48 -0700

    .

--- app/controllers/api/v2/punchlists_controller.rb
+++ app/controllers/api/v2/punchlists_controller.rb
@@ -1,13 +1,19 @@
 class Api::V2::PunchlistsController < Api::V2::ApiController
 
-    def show
-        ## should remove params[:id] component after API compatibility issues are solved
-        if params[:project_id].present?
-            project = Project.find params[:project_id]
-        else
-        	project = Project.find params[:id]
+    def index
+        project = Project.find params[:project_id]
+        if project.punchlists && project.punchlists.count > 0
+            punchlist = project.punchlists.first
+        else 
+            punchlist = project.punchlists.create
         end
+        respond_to do |format|
+            format.json { render_for_api :punchlist, :json => punchlist, :root => :punchlist}
+        end
+    end
 
+    def show
+        project = Project.find params[:id]
     	if project.punchlists && project.punchlists.count > 0
     		punchlist = project.punchlists.first
     	else 
