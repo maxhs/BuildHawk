cb8a68148adaf23e5e287a3b375dcd7dd61ee3fb
Max <max@foodia.org>
2014-05-29 12:55:28 -0700

    .

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -135,8 +135,8 @@ class Photo < ActiveRecord::Base
 		t.add :user_name
 		t.add :name
 		t.add :created_date
-		#t.add :folder_name, :if => :has_folder?
-		#t.add :folder_id, :if => :has_folder?
+		t.add :folder_name
+		t.add :folder_id
 		#t.add :assignee, :if => :has_assignee?
 		##slated for removal
 		t.add :url200
