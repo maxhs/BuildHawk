428ddc6797849ed3f6f047b310e90818b2e6c247
Max <max@foodia.org>
2014-05-29 13:48:00 -0700

    .

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,7 +4,7 @@ require 'resque_scheduler/server'
 
 #uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
 #Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
 Resque.redis = Redis.new(:url => ENV['REDISTOGO_URL'])
 Resque.redis.namespace = "resque:buildhawk-rails"
 
