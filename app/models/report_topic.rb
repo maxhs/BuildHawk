a48ef48fd8700bcc2493ef2bebd0e94231ea37c9
Max <max@foodia.org>
2014-06-03 17:16:49 -0700

    .

--- app/models/report_topic.rb
+++ app/models/report_topic.rb
@@ -2,4 +2,12 @@ class ReportTopic < ActiveRecord::Base
 	attr_accessible :report_id, :safety_topic_id
 	belongs_to :report, autosave: true
 	belongs_to :safety_topic, autosave: true
+
+	acts_as_api
+
+  	api_accessible :report do |t|
+  		t.add :id
+  		t.add :report_id
+  		t.add :safety_topic_id
+  	end
 end
