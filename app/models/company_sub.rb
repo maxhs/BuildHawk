9d1e86a166e4b198ba038254d3d9cbb88e629ad9
Max <max@foodia.org>
2014-06-05 09:32:42 -0700

    .

--- app/models/company_sub.rb
+++ app/models/company_sub.rb
@@ -2,4 +2,11 @@ class CompanySub < ActiveRecord::Base
 	attr_accessible :company_id, :subcontractor_id
 	belongs_to :company
 	belongs_to :subcontractor, :class_name => "Company"
+
+	acts_as_api
+
+    api_accessible :report do |t|
+        t.add :id
+        t.add :subcontractor
+    end
 end
