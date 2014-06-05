5a87a79b2dbf152fc472abbfe1dd7a21f5f3cf59
Max <max@foodia.org>
2014-06-05 13:50:39 -0700

    .

--- app/controllers/api/v2/company_subs_controller.rb
+++ app/controllers/api/v2/company_subs_controller.rb
@@ -4,7 +4,8 @@ class Api::V2::CompanySubsController < Api::V2::ApiController
 		company = Company.find params[:company_id]
 		name = "#{params[:subcontractor][:name]}"
 		subcontractor = Company.where("name ILIKE ?",name).first_or_create
-		company.company_subs.create :subcontractor => subcontractor
+		subcontractor.update_attributes params[:subcontractor] 
+		company.company_subs.create :subcontractor_id => subcontractor.id
     	respond_to do |format|
         	format.json { render_for_api :report, :json => company, :root => :company}
       	end
