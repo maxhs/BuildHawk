5b33def1f9d200b1d47fe22a695607ffb0255622
Max <max@foodia.org>
2014-06-06 09:09:01 -0700

    .

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -134,6 +134,7 @@ class Photo < ActiveRecord::Base
 		t.add :created_at
 		t.add :user_name
 		t.add :name
+		t.add :description
 		t.add :created_date
 		t.add :folder_name, :if => :has_folder?
 		t.add :folder_id, :if => :has_folder?
