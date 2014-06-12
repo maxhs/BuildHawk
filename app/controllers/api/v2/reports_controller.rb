4132d846dac878689082622438a64205559e2663
Max <max@foodia.org>
2014-06-12 06:57:44 -0700

    .

--- app/controllers/api/v2/reports_controller.rb
+++ app/controllers/api/v2/reports_controller.rb
@@ -56,10 +56,10 @@ class Api::V2::ReportsController < Api::V2::ApiController
 
         if params[:report][:safety_topics].present?
             params[:report][:safety_topics].each do |topic|
-                if topic["topic_id"]
-                    report.report_topics.where(:safety_topic_id => topic["id"]).first_or_create
+                if topic["id"]
+                    @report.safety_topics.where(:safety_topic_id => topic["id"]).first_or_create
                 else
-                    report.report_topics.where(:title => topic["title"], :info => topic["info"], :company_id => @current_user.company.id).first_or_create
+                    @report.report_topics.where(:title => topic["title"]).first_or_create
                 end
             end
             params[:report].delete(:safety_topics)
@@ -141,12 +141,12 @@ class Api::V2::ReportsController < Api::V2::ApiController
         @report = Report.create params[:report]
         @report.update_attribute :mobile, true
         
-        if topics && topics.count > 0
+        if topics
             topics.each do |topic|
-                if topic["topic_id"]
-                    @report.report_topics.where(:safety_topic_id => topic["id"]).first_or_create
+                if topic["id"]
+                    @report.safety_topics.where(:safety_topic_id => topic["id"]).first_or_create
                 else
-                    @report.report_topics.where(:title => topic["title"], :info => topic["info"], :company_id => @current_user.company.id).first_or_create
+                    @report.report_topics.where(:title => topic["title"]).first_or_create
                 end
             end
         end
