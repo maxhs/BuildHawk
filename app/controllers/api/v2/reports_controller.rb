1f64d411161349c32f4b9c651322a8c00f1c8e34
Max <max@foodia.org>
2014-06-12 07:01:31 -0700

    .

--- app/controllers/api/v2/reports_controller.rb
+++ app/controllers/api/v2/reports_controller.rb
@@ -57,9 +57,10 @@ class Api::V2::ReportsController < Api::V2::ApiController
         if params[:report][:safety_topics].present?
             params[:report][:safety_topics].each do |topic|
                 if topic["id"]
-                    @report.safety_topics.where(:safety_topic_id => topic["id"]).first_or_create
+                    @report.report_topics.where(:safety_topic_id => topic["id"]).first_or_create
                 else
-                    @report.report_topics.where(:title => topic["title"]).first_or_create
+                    new_topic = @report.project.company.safety_topics.where(:title => topic["title"]).first_or_create
+                    @report.report_topics.create(:safety_topic_id => new_topic.id)
                 end
             end
             params[:report].delete(:safety_topics)
@@ -144,9 +145,10 @@ class Api::V2::ReportsController < Api::V2::ApiController
         if topics
             topics.each do |topic|
                 if topic["id"]
-                    @report.safety_topics.where(:safety_topic_id => topic["id"]).first_or_create
+                    @report.report_topics.where(:safety_topic_id => topic["id"]).first_or_create
                 else
-                    @report.report_topics.where(:title => topic["title"]).first_or_create
+                    new_topic = @report.project.company.safety_topics.where(:title => topic["title"]).first_or_create
+                    @report.report_topics.create(:safety_topic_id => new_topic.id)
                 end
             end
         end
