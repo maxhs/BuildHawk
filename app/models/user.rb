782e4c1c5287cf72c27513d194fe0fb9e598f702
Max <max@foodia.org>
2014-06-19 14:04:06 -0700

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,12 +59,14 @@ class User < ActiveRecord::Base
         end
     end
 
-    def text_task(tasK)
+    def text_task(task)
+        clean_phone
         @account_sid = 'AC9876d738bf527e6b9d35af98e45e051f'
         @auth_token = '217b868c691cd7ec356c7dbddb5b5939'
         twilio_phone = "14157234334"
         @client = Twilio::REST::Client.new(@account_sid, @auth_token)
         truncated_task = truncate(task.body, length:15)
+        puts "should be sending a task text, #{truncated_task}, to #{full_name} at phone: #{phone}"
         @client.account.sms.messages.create(
             :from => "+1#{twilio_phone}",
             :to => phone,
