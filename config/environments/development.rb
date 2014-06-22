25f418d09748566031409bbcca1c6c80c102878d
Max <max@foodia.org>
2014-06-22 10:08:02 -0700

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,13 +32,4 @@ Buildhawk::Application.configure do
   # Configure mailer SMTP settings
   config.action_mailer.delivery_method = :smtp
   
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
-
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,15 +78,6 @@ Buildhawk::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
-  
   #for the mailer
   config.action_mailer.default_url_options = { :host => 'www.buildhawk.com' }
   config.action_mailer.asset_host = "http://www.buildhawk.com"
