9d8aaebc236d94afe043a398f9f76a0589c6d4aa
Max <max@foodia.org>
2014-06-26 10:01:24 -0700

    .

--- app/controllers/api/v2/reminders_controller.rb
+++ app/controllers/api/v2/reminders_controller.rb
@@ -4,7 +4,7 @@ class Api::V2::RemindersController < Api::V2::ApiController
 		params[:reminder][:reminder_datetime] = Time.at(params[:date].to_i).to_datetime
 		reminder = Reminder.create params[:reminder]
 		if reminder.save
-			render json: {success: true}
+			render json: {reminder: reminder}
 		else
 			render json: {failure: false}
 		end
