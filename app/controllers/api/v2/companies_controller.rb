adee2eadd2d7818bca9dd77bc1fe48b0f4b4d86c
Max <max@foodia.org>
2014-06-26 14:48:10 -0700

    .

--- app/controllers/api/v2/companies_controller.rb
+++ app/controllers/api/v2/companies_controller.rb
@@ -1,5 +1,10 @@
 class Api::V2::CompaniesController < Api::V2::ApiController
 
-	
+	def show
+		company = Company.find params[:id]
+    	respond_to do |format|
+        	format.json { render_for_api :projects, :json => company, :root => :company}
+      	end
+	end	
 
 end
--- app/jobs/setreminder.rb
+++ app/jobs/setreminder.rb
@@ -4,8 +4,8 @@ module SetReminder
 	def self.perform(reminder_id)
 		reminder = Reminder.where(:id => reminder_id).last
 		puts "Enqueuing a reminder: #{reminder}"
-		#if reminder.user && reminder.user.email_permissions
+		if reminder.user && reminder.user.email_permissions
 			ReminderMailer.checklist_reminder(reminder_id).deliver
-		#end
+		end
 	end
 end
