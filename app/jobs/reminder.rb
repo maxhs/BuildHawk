c9fbe494925a3b6d3e9c98c98605df6944fc18af
Max <max@foodia.org>
2014-06-26 11:28:55 -0700

    .

--- app/jobs/reminder.rb
+++ app/jobs/reminder.rb
@@ -2,7 +2,7 @@ module Reminder
 	@queue = :reminder
 
 	def self.perform(reminder_id)
-		reminder = Reminder.find reminder_id
+		reminder = Reminder.where(:id => reminder_id).last
 		puts "Enqueuing a reminder: #{reminder}"
 		#if reminder.user && reminder.user.email_permissions
 			ReminderMailer.checklist_reminder(reminder_id).deliver
