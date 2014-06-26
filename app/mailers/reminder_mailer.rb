111ad01fa16ea3d72762fd8f19f73205fdea9a38
Max <max@foodia.org>
2014-06-26 11:44:45 -0700

    .

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -2,8 +2,8 @@ class ReminderMailer < ActionMailer::Base
     layout "list_mailer"
 
     def checklist_reminder(reminder_id)
-      	@recipient = User.where(:email => recipient_email).first
       	@reminder = Reminder.find reminder_id
+      	@recipient = @reminder.user
       	@logo_url = @company.image.url(:small) if @company.image_file_name
       	@checklist_item = reminder.checklist_item
       	@project = @checklist_item.checklist.project
