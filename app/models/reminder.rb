7d8e91e4abdc764c926560172715712f6a65e3ed
Max <max@foodia.org>
2014-06-26 10:16:34 -0700

    .

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -16,4 +16,41 @@ class Reminder < ActiveRecord::Base
 
 	end
 
+	acts_as_api
+
+
+	api_accessible :projects do |t|
+		t.add :id
+		t.add :user
+		t.add :checklist_item
+		t.add :reminder_datetime
+		t.add :email
+		t.add :text
+		t.add :push
+		t.add :active
+	end
+
+	api_accessible :user, :extend => :projects do |t|
+      
+    end
+
+    api_accessible :company, :extend => :projects do |t|
+      
+    end
+
+    api_accessible :worklist, :extend => :projects do |t|
+      
+    end
+
+    api_accessible :checklist, :extend => :projects do |t|
+      
+    end
+
+    api_accessible :details, :extend => :projects do |t|
+      
+    end
+    api_accessible :detail, :extend => :projects do |t|
+      
+    end
+
 end
