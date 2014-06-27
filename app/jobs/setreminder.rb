75641a43ab7e5cdecd0eb792a327e1f17eb063d8
Max <max@foodia.org>
2014-06-26 18:35:56 -0700

    .

--- app/jobs/setreminder.rb
+++ app/jobs/setreminder.rb
@@ -6,7 +6,7 @@ module SetReminder
 		if reminder.user && reminder.user.push_permissions
 			reminder.user.notifications.create(
 				:checklist_item_id => reminder.checklist_item.id,
-				:body => "Checklist reminder: #{reminder.checklist_item.body[0..20]}..."
+				:body => "Checklist reminder: #{reminder.checklist_item.body[0..20]}...",
 				:notification_type => "Reminder"
 			)
 			#ReminderMailer.checklist_reminder(reminder_id).deliver
