78c21d5c28c525cdc8146330f77b2603026d5335
Max <max@foodia.org>
2014-06-26 18:06:51 -0700

    .

--- app/models/checklist_item.rb
+++ app/models/checklist_item.rb
@@ -75,7 +75,7 @@ class ChecklistItem < ActiveRecord::Base
 
             category.update_attribute :completed_date, Time.now if category.completed_count == category.item_count    
         else
-            self.update_attribute :completed_date, nil
+            #self.update_attribute :completed_date, nil
         end
     end
 
