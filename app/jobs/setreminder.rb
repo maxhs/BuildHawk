2490bacdf5ea8df2afba5b6eff51abe1f61047b1
Max <max@foodia.org>
2014-07-02 09:20:32 -0700

    .

--- app/jobs/setreminder.rb
+++ app/jobs/setreminder.rb
@@ -4,12 +4,14 @@ module SetReminder
 	def self.perform(reminder_id)
 		reminder = Reminder.where(:id => reminder_id).last
 		if reminder.user && reminder.user.push_permissions
-			reminder.user.notifications.create(
-				:checklist_item_id => reminder.checklist_item.id,
-				:body => "Checklist reminder: #{reminder.checklist_item.body[0..20]}...",
-				:notification_type => "Reminder"
-			)
-			#ReminderMailer.checklist_reminder(reminder_id).deliver
+			if reminder.checklist_item
+				reminder.user.notifications.create(
+					:checklist_item_id => reminder.checklist_item.id,
+					:body => "Checklist reminder: #{reminder.checklist_item.body[0..20]}...",
+					:notification_type => "Reminder"
+				)
+				#ReminderMailer.checklist_reminder(reminder_id).deliver
+			end
 		end
 	end
 end
