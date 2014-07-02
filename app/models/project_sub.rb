847256357c295b1162bf238694cae7645556c91a
Max <max@foodia.org>
2014-07-02 12:30:25 -0700

    .

--- app/models/project_sub.rb
+++ app/models/project_sub.rb
@@ -1,14 +1,14 @@
 class ProjectSub < ActiveRecord::Base
-	attr_accessible :project_id, :company_sub_id
+	attr_accessible :project_id, :company_id
 	belongs_to :project
-	belongs_to :company_sub
-	validates_uniqueness_of :project_id, :scope => :company_sub_id
+	belongs_to :company
+	validates_uniqueness_of :project_id, :scope => :company_id
 
 	acts_as_api
 
   	api_accessible :projects do |t|
         t.add :id
-        t.add :company_sub
+        t.add :company
     end
     
     api_accessible :worklist, :extend => :projects do |t|
