771aa163e0e18bb9d8e8052046a4dd0b9236dca3
Max <max@foodia.org>
2014-07-02 21:26:37 -0700

    .

--- app/controllers/api/v2/checklist_items_controller.rb
+++ app/controllers/api/v2/checklist_items_controller.rb
@@ -3,6 +3,16 @@ class Api::V2::ChecklistItemsController < Api::V2::ApiController
     def update
     	item = ChecklistItem.find params[:id]
     	item.update_attributes params[:checklist_item]
+        if params[:user_id]
+            user = User.find params[:user_id]
+            if item.status.length
+                activities.create(
+                    :body => "#{user.full_name} updated the status for this item to \"#{status}\".",
+                    :project_id => item.checklist.project.id,
+                    :activity_type => item.class.name
+                )
+            end
+        end
     	respond_to do |format|
         	format.json { render_for_api :details, :json => item, :root => :checklist_item}
       	end
