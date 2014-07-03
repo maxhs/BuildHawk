1dc3eaab931143274df067fb57b4f4931efc2be0
Max <max@foodia.org>
2014-07-02 18:43:30 -0700

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
     has_many :message_users, :dependent => :destroy, autosave: true
     has_many :messages, :through => :message_users , autosave: true
 
+    has_many :reminders, :dependent => :destroy
     has_many :alternates, :dependent => :destroy
     has_many :activities#, :dependent => :destroy
 
