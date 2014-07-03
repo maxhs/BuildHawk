de5277425650e9f55efdcaa948069b74a341e10e
Max <max@foodia.org>
2014-07-02 18:57:22 -0700

    .

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-	attr_accessible :author_id, :company_id, :target_project_id, :body, :recipient_ids
+	attr_accessible :author_id, :company_id, :target_project_id, :body, :user_ids
 
 	belongs_to :author
 	belongs_to :company
@@ -13,21 +13,30 @@ class Message < ActiveRecord::Base
 	def notify
 		users.each do |u|
 			u.notifications.create(
-				:alert 				=> body,
+				:body 				=> body,
 				:message_id 		=> id,
+				:company_id			=> company_id,
 				:project_id 		=> target_project_id,
-				:notification_type 	=> "Message"
+				:notification_type 	=> self.class.name
 			)
 		end
 	end
 
 	acts_as_api
 
-	api_accessible :reports do |t|
+	api_accessible :notifications do |t|
         t.add :id
         t.add :body
         t.add :company
         t.add :target_project
         t.add :comments
+    end
+
+    api_accessible :projects, :extend => :notifications do |t|
+
+    end
+
+    api_accessible :details, :extend => :notifications do |t|
+    	
     end
 end
