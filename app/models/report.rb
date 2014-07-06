ea404c79dbd6dee076b54845c01a8d9d78d9298f
Max <max@foodia.org>
2014-07-05 17:56:25 -0700

    .

--- app/models/report.rb
+++ app/models/report.rb
@@ -172,10 +172,7 @@ class Report < ActiveRecord::Base
         t.add :humidity
         t.add :report_fields
         t.add :possible_types
-        t.add :comments
         t.add :photos
-        t.add :report_users
-        t.add :report_companies
         t.add :report_topics
         t.add :body, :if => :has_body?
     end
