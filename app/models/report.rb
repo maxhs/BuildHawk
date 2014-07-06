cdf73cc9aa4f74b330c5b4c4dae37897435c9e85
Max <max@foodia.org>
2014-07-05 19:35:02 -0700

    .

--- app/models/report.rb
+++ app/models/report.rb
@@ -163,9 +163,9 @@ class Report < ActiveRecord::Base
         t.add :possible_types
         t.add :comments
         t.add :photos
-        t.add :report_users
-        t.add :report_companies
-        t.add :report_topics
+        #t.add :report_users
+        #t.add :report_companies
+        #t.add :report_topics
         t.add :body, :if => :has_body?
 
         ### slated for deletion in next version. replace epoch_time with created_date as soon as 1.04 is out ###
