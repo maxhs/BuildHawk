3ad3e03a5d5a0d4be11918d52b825278526fd530
Max <max@foodia.org>
2014-07-07 16:31:28 -0700

    .

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,9 +2,15 @@ class Lead < ActiveRecord::Base
 	attr_accessible :name, :company_name, :full_name, :email, :phone
 
 	after_create :email_will
+	after_create :clean_phone
 
 	def email_will
 		LeadMailer.email_will(self).deliver
+	end
+
+	def clean_phone
+        phone = phone.gsub(/[^0-9a-z ]/i, '').gsub(/\s+/,'')
+        self.save
 	end
   	
 end
