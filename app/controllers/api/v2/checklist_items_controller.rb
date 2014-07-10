ea0dafcfabf7575f9a2a155ebfa3324b21c0eca8
Max <max@foodia.org>
2014-07-10 14:00:11 -0700

    .

--- app/controllers/api/v2/checklist_items_controller.rb
+++ app/controllers/api/v2/checklist_items_controller.rb
@@ -32,9 +32,9 @@ class Api::V2::ChecklistItemsController < Api::V2::ApiController
         if should_log_activity
             if params[:user_id]
                 user = User.find params[:user_id]
-                item.log_activity(user) if user
+                #item.log_activity(user) if user
             else
-                item.log_activity(nil)
+                #item.log_activity(nil)
             end
         end
 
