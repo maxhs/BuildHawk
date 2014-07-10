43c98b26ee60ae8e92ba4e11585bbc75acc48c73
Max <max@foodia.org>
2014-07-10 14:41:02 -0700

    .

--- app/controllers/api/v2/companies_controller.rb
+++ app/controllers/api/v2/companies_controller.rb
@@ -2,6 +2,7 @@ class Api::V2::CompaniesController < Api::V2::ApiController
 
 	def show
 		company = Company.find params[:id]
+    	render json: {company: {subcontractors: company.subcontractors}}
     	respond_to do |format|
         	format.json { render_for_api :projects, :json => company, :root => :company}
       	end
