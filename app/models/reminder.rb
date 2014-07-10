994e2ad0385d6610f34cb2806e90c0d0c6c13ee3
Max <max@foodia.org>
2014-07-09 21:31:25 -0700

    .

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -14,23 +14,23 @@ class Reminder < ActiveRecord::Base
 	default_scope { order('reminder_datetime') }
 
 	def schedule
-		Activity.create(
-			:checklist_item_id => checklist_item_id,
-			:worklist_item_id => worklist_item_id,
-			:project_id => project_id,
-			:user_id => user_id,
-			:activity_type => self.class.name,
-			:body => "#{user.full_name} set a reminder for #{reminder_datetime.strftime("%b%e,%l:%M %p")}."
-		)
+		# Activity.create(
+		# 	:checklist_item_id => checklist_item_id,
+		# 	:worklist_item_id => worklist_item_id,
+		# 	:project_id => project_id,
+		# 	:user_id => user_id,
+		# 	:activity_type => self.class.name,
+		# 	:body => "#{user.full_name} set a reminder for #{reminder_datetime.strftime("%b%e,%l:%M %p")}."
+		# )
 		Resque.enqueue_at(reminder_datetime, SetReminder, id)
 	end
 
 	def unschedule
-		if checklist_item
-			Activity.where(:activity_type => self.class.name, :checklist_item_id => checklist_item_id, :user_id => user_id).each do |a| a.destroy end
-		elsif worklist_item
-			Activity.where(:activity_type => self.class.name, :worklist_item_id => worklist_item_id, :user_id => user_id).each do |a| a.destroy end
-		end
+		# if checklist_item
+		# 	Activity.where(:activity_type => self.class.name, :checklist_item_id => checklist_item_id, :user_id => user_id).each do |a| a.destroy end
+		# elsif worklist_item
+		# 	Activity.where(:activity_type => self.class.name, :worklist_item_id => worklist_item_id, :user_id => user_id).each do |a| a.destroy end
+		# end
 		Resque.remove_delayed(SetReminder,id)
 	end
 
