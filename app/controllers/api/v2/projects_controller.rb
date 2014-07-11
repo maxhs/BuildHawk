1818e8853f812a1169a4fbf17983d352c9eb2704
Max <max@foodia.org>
2014-07-10 22:14:48 -0700

    .

--- app/controllers/api/v2/projects_controller.rb
+++ app/controllers/api/v2/projects_controller.rb
@@ -2,7 +2,7 @@ class Api::V2::ProjectsController < Api::V2::ApiController
 
     def index
         user = User.find params[:user_id]
-        projects = user.project_users.where("archived = ? and core = ? and project_group_id IS NULL",false,false).map(&:project).compact.sort_by{|p| p.name.downcase}
+        projects = user.project_users.where("archived = ? and core = ? and project_group_id IS NULL",false,false).map{|p| p.project if p.project.company_id == user.company_id}.compact.sort_by{|p| p.name.downcase}
         if projects
         	respond_to do |format|
             	format.json { render_for_api :projects, :json => projects, :root => :projects}
