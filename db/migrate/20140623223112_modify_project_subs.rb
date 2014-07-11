07b9f2e3c6789ad0add6a36ba6d7aaf5e86a325c
Max <max@foodia.org>
2014-07-10 23:15:54 -0700

    .

--- db/migrate/20140623223112_modify_project_subs.rb
+++ db/migrate/20140623223112_modify_project_subs.rb
@@ -1,7 +1,7 @@
 class ModifyProjectSubs < ActiveRecord::Migration
   def change
   	rename_column :project_subs, :sub_id, :company_sub_id
-  	rename_table :edits, :activities
+  	#rename_table :edits, :activities
   	add_column :activities, :activity_type, :string
   	add_column :activities, :comment_id, :integer
   	add_column :activities, :message_id, :integer
