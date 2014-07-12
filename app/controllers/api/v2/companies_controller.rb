22a7345720c7dcb302cd5f7e0910bf8e4baa7f29
Max <max@foodia.org>
2014-07-12 13:11:09 -0700

    .

--- app/controllers/api/v2/companies_controller.rb
+++ app/controllers/api/v2/companies_controller.rb
@@ -22,7 +22,7 @@ class Api::V2::CompaniesController < Api::V2::ApiController
 	end
 
 	def add
-		company = Company.create name: params[:name]
+		company = Company.where(:name => params[:name]).first_or_create
 		project = Project.find params[:project_id]
 		project.companies << company unless project.companies.flatten.include?(company)
 		render json: {company: company}
