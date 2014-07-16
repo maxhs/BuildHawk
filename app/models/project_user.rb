bea0eb75b1d8ba1990c454fcfcf734068ffec3ff
Max <max@foodia.org>
2014-07-16 00:13:55 -0700

    .

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -3,7 +3,7 @@ class ProjectUser < ActiveRecord::Base
 	belongs_to :project
 	belongs_to :user
 	belongs_to :connect_user
-	validates_uniqueness_of :project_id, :scope => :user_id
+	#validates_uniqueness_of :project_id, :scope => :user_id
 
 	after_create :notify
 
