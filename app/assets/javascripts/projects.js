56e93e5732f5937c8b3e67e08c7e3b99cfe15474
Max <max@foodia.org>
2014-07-18 18:46:08 -0700

    .

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -10,9 +10,8 @@ function sidebarSetup(){
 
 function newProjectSetup(){
 	$('.new_project').submit(function(e){
-		console.log("checklist: "+$('#checklist_id').val() == null);
 		if ($('#checklist_id').val()) {
-			$('#alert').text("Creating your template. This may take a few moments.").fadeIn(500);
+			$('#alert').text("Creating your project. This may take a few moments.").fadeIn(500);
 		} else {
 			$('#alert').html("<span>Please select a checklist. You can create checklist templates <a href='/admin/checklists' id='alert-refresh' style='text-decoration:underline;' data-remote=true >here</a></span>.").fadeIn(200);
 			return false;
