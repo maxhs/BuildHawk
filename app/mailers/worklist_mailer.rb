8d005bd6cd133e12771c95fed350bb2651218e0c
Max <max@foodia.org>
2014-07-24 00:49:57 -0700

    .

--- app/mailers/worklist_mailer.rb
+++ app/mailers/worklist_mailer.rb
@@ -4,7 +4,10 @@ class WorklistMailer < ActionMailer::Base
   	def export(recipient_email, item_array, project)
   		@recipient = User.where(:email => recipient_email).first
   		@recipient = Sub.where(:email => recipient_email).first unless @recipient
-      @recipient = ConnectUser.where(:email => recipient_email).first unless @recipient
+      unless @recipient
+        @recipient = ConnectUser.where(:email => recipient_email).first
+        @connect_user = @recipient if @recipient
+      end
   		@project = project
       @company = @project.company
   		@item_array = item_array
