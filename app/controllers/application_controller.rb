6dcccd485abb1c1655e64c0ce1c397dfe9c94e76
Max <max@foodia.org>
2014-07-24 02:19:32 -0700

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
             @mobile_redirect = true
             if params[:controller] == "worklist_items" && params[:id]
                 @item = WorklistItem.find params[:id]
-                render layout: "mobile" and return
+                #render layout: "mobile" and return
             end
         end 
     end
