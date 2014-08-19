a05799a48195d4af1bea8f0cd70fcb4d049e1222
Max <max@foodia.org>
2014-08-18 18:01:20 -0700

    .

---
+++ app/controllers/app_controller.rb
@@ -0,0 +1,18 @@
+class AppController < ApplicationController
+	before_filter :user_projects
+
+	layout 'app'
+
+    def user_projects
+        if user_signed_in?
+            if current_user.any_admin?
+                @projects = current_user.company.projects
+                @archived_projects = current_user.project_users.where(:archived => true).map(&:project)
+            else
+                @projects = current_user.project_users.where(:archived => false).map{|p| p.project if p.project.company_id == current_user.company_id}.compact.uniq
+                @archived_projects = current_user.project_users.where(:archived => true).map(&:project).compact.uniq
+            end
+        end
+    end
+
+end
