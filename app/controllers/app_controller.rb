1c876c3ca5725ff2684d0b3b239d4405fc659196
Max <max@foodia.org>
2014-09-08 13:32:24 -0700

    .

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -8,11 +8,10 @@ class AppController < ApplicationController
                 @projects = current_user.company.projects    
                 @archived_projects = current_user.project_users.where(:archived => true).map(&:project)
             else
-                @projects = current_user.project_users.where("archived = ? and project_group_id IS NULL",false).map{|p| p.project if p.project.company_id == current_user.company_id}.compact.uniq
+                @projects = current_user.project_users.where("archived = ?",false).map{|p| p.project if p.project.company_id == current_user.company_id}.compact.uniq
                 @archived_projects = current_user.project_users.where(:archived => true).map(&:project).compact.uniq
             end
             
-            @groups = current_user.company.project_groups.map{|g|g if g.projects.count > 0}.compact
             @items = current_user.connect_items(nil)
             @companies = @items.map{|t| t.worklist.project.company}.uniq
         end
