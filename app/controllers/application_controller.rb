a5589745bbdf669963e8d8c8c61a994a16915126
Max <max@foodia.org>
2014-09-08 14:12:34 -0700

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
     #after_filter :store_location
-    #before_filter :detect_redirect
-    #before_filter :configure_permitted_parameters, if: :devise_controller?
+    before_filter :detect_redirect
+    before_filter :configure_permitted_parameters, if: :devise_controller?
 
     def detect_redirect
         if params[:m]
