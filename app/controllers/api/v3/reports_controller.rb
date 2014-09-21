c33e6163de4fab94896303e0253bdd05e2f7acc3
Max <max@foodia.org>
2014-09-21 13:57:53 -0700

    .

--- app/controllers/api/v3/reports_controller.rb
+++ app/controllers/api/v3/reports_controller.rb
@@ -172,7 +172,7 @@ class Api::V3::ReportsController < Api::V3::ApiController
             end
             topics_for_deletion = report.report_topics - new_topics
             puts "topics for deletion: #{topics_for_deletion}"
-            topics_for_deletion.destroy_all
+            topics_for_deletion.each(&:destroy)
             params[:report].delete(:safety_topics)
         end
 
