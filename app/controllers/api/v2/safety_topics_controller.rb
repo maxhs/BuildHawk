aca3dd5b1cf5a7257244520e1959c290d65efff6
Max <max@foodia.org>
2014-09-21 12:58:43 -0700

    .

--- app/controllers/api/v2/safety_topics_controller.rb
+++ app/controllers/api/v2/safety_topics_controller.rb
@@ -2,10 +2,16 @@ class Api::V2::SafetyTopicsController < Api::V2::ApiController
 	
 	def destroy
 		topic = ReportTopic.find params[:id]
-		if topic.destroy
-			render json: { success: true }
-		else
-			render json: { failure: false }
+		report = Report.find params[:report_id]
+		if topic && report
+
+			if report.report_topics.include? topic && topic.destroy
+				render json: { success: true }
+			else
+				render json: { success: false }
+			end
+		else 
+			render json: { success: false }
 		end
 	end
 
