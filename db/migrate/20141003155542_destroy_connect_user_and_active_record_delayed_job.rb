0cdfc5d734cc32250e86d07313f4c62d7bc269f5
Max <max@foodia.org>
2014-10-03 09:32:19 -0700

    .

--- db/migrate/20141003155542_destroy_connect_user_and_active_record_delayed_job.rb
+++ db/migrate/20141003155542_destroy_connect_user_and_active_record_delayed_job.rb
@@ -3,7 +3,7 @@ class DestroyConnectUserAndActiveRecordDelayedJob < ActiveRecord::Migration
   		remove_column :report_users, :connect_user_id, :integer
   		remove_column :task_users, :connect_user_id, :integer
   		remove_column :tasks, :connect_assignee_id, :integer
-  		#drop_table :connect_users
-  		#drop_table :delayed_jobs
+  		drop_table :connect_users
+  		drop_table :delayed_jobs
   	end
 end
