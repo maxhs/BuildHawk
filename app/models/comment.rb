66124c50678a384870c79c2eb3b52458025b7cec
Max <max@foodia.org>
2014-10-03 23:03:03 -0700

    .

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,7 +12,7 @@ class Comment < ActiveRecord::Base
 
     validates_presence_of :body
     validates :body, :length => { :minimum => 1 }
-    default_scope { order('created_at ASC') }
+    default_scope { order('created_at DESC') }
 
     after_commit :notify, on: :create
 
