4ec7bd1de52ea2eebda6e47cdba380d5ceb4e463
Max <max@foodia.org>
2014-10-07 16:25:21 -0700

    .

--- app/controllers/api/v3/groups_controller.rb
+++ app/controllers/api/v3/groups_controller.rb
@@ -2,13 +2,14 @@ class Api::V3::GroupsController < Api::V3::ApiController
 
     def index
         @user = User.find params[:user_id]
-        group_ids = @user.project_users.where("project_group_id IS NOT NULL and archived = ? and core = ?",false,false).map(&:project_group_id).uniq
-        groups = []
-        if group_ids.count > 0
-            group_ids.each do |g|
-                groups << ProjectGroup.find(g)
-            end 
-        end
+        groups = @user.company.project_groups
+        # group_ids = @user.company.project_users.where("project_group_id IS NOT NULL and archived = ? and core = ?",false,false).map(&:project_group_id).uniq
+        # groups = []
+        # if group_ids.count > 0
+        #     group_ids.each do |g|
+        #         groups << ProjectGroup.find(g)
+        #     end 
+        # end
 
         if groups.count > 0
             respond_to do |format|
