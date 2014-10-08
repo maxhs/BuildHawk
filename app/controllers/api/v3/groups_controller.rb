b0bbb82dfb4dec051230d88892ff4093adef0d12
Max <max@foodia.org>
2014-10-08 14:54:54 -0700

    .

--- app/controllers/api/v3/groups_controller.rb
+++ app/controllers/api/v3/groups_controller.rb
@@ -1,13 +1,15 @@
 class Api::V3::GroupsController < Api::V3::ApiController
 
     def index
-        @user = User.find params[:user_id]
+        user = User.find params[:user_id]
         groups = @user.company.project_groups
 
         if groups.count > 0
-            respond_to do |format|
-                format.json { render_for_api :details, :json => groups, :root => :groups}
-            end
+
+            render json: {groups: groups.map{|group| {id: group.id, name: group.name, projects_count: group.projects_count, projects: group.projects.map{|project| {id: project.id, name: project.name, archived: project.archived_for_user?(user) } } } }
+            #respond_to do |format|
+            #    format.json { render_for_api :details, :json => groups, :root => :groups}
+            #end
         else
             render json: {success: false}
         end
