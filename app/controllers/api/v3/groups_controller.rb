8c3a67bffa2083da76a6ecd26db1248ede40478a
Max <max@foodia.org>
2014-10-08 16:13:17 -0700

    .

--- app/controllers/api/v3/groups_controller.rb
+++ app/controllers/api/v3/groups_controller.rb
@@ -5,7 +5,7 @@ class Api::V3::GroupsController < Api::V3::ApiController
         groups = user.company.project_groups
 
         if groups.count > 0
-            render json: {groups: groups.map{|group| {id: group.id, name: group.name, projects_count: group.projects_count, projects: group.projects.map{|project| {id: project.id, name: project.name, address: {id: project.address.id, formatted_address: project.address.formatted_address}, users: project.users.map{|user| {id:user.id}}, hidden: project.hidden_for_user?(user) } } } } }
+            render json: {groups: groups.map{|group| {id: group.id, name: group.name, projects_count: group.projects_count, projects: group.projects.map{|project| {id: project.id, name: project.name, address: {id: project.address.id, formatted_address: project.address.formatted_address}, progress: project.progress, users: project.users.map{|user| {id:user.id}}, hidden: project.hidden_for_user?(user) } } } } }
         else
             render json: {success: false}
         end
