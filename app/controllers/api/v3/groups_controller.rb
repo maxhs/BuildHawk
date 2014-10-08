d79da963a2823439ff4551da5e00066de71cf1ae
Max <max@foodia.org>
2014-10-08 15:59:21 -0700

    .

--- app/controllers/api/v3/groups_controller.rb
+++ app/controllers/api/v3/groups_controller.rb
@@ -6,9 +6,6 @@ class Api::V3::GroupsController < Api::V3::ApiController
 
         if groups.count > 0
             render json: {groups: groups.map{|group| {id: group.id, name: group.name, projects_count: group.projects_count, projects: group.projects.map{|project| {id: project.id, name: project.name, address: project.address, users: project.users.map{|user| {id:user.id}}, hidden: project.hidden_for_user?(user) } } } } }
-            #respond_to do |format|
-            #    format.json { render_for_api :details, :json => groups, :root => :groups}
-            #end
         else
             render json: {success: false}
         end
@@ -17,10 +14,7 @@ class Api::V3::GroupsController < Api::V3::ApiController
     def show
     	group = ProjectGroup.find params[:id]
         user = User.find params[:user_id]
-    	render json: {id: group.id, name: group.name, projects: group.projects.map{|project| {name: project.name, hidden: project.hidden_for_user?(user), address: project.address} } }
-        #respond_to do |format|
-        #	format.json { render_for_api :details, :json => group, :root => :group}
-      	#end
+    	render json: {id: group.id, name: group.name, projects: group.projects.map{|project| {name: project.name, hidden: project.hidden_for_user?(user), address: {formatted_address: project.address.formatted_address} } } }
     end
 
     def destroy
