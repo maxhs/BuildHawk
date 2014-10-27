559790372fcfb255eea85a6a1722ece391f18d2b
Max <max@foodia.org>
2014-10-27 14:22:43 -0700

    .

--- app/models/error.rb
+++ app/models/error.rb
@@ -1,5 +1,5 @@
 class Error < ActiveRecord::Base
-	attr_accessible :user_id, :report_id, :checklist_item_id, :photo_id, :task_id, :message_id. :body, :status_code
+	attr_accessible :user_id, :report_id, :checklist_item_id, :photo_id, :task_id, :message_id, :body, :status_code
 
 	belongs_to :user
 	belongs_to :report
