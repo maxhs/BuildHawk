6c880b7dec84c65c61dd9673401655177626870e
Max <max@foodia.org>
2014-10-27 13:58:38 -0700

    .

--- db/migrate/20141015224101_add_mobile_id_to_photo_object.rb
+++ db/migrate/20141015224101_add_mobile_id_to_photo_object.rb
@@ -1,6 +1,6 @@
 class AddMobileIdToPhotoObject < ActiveRecord::Migration
   	def change
   		add_column :photos, :taken_at, :datetime
-  		add_column :users, :mobile_token, :string
+  		#add_column :users, :mobile_token, :string
   	end
 end
