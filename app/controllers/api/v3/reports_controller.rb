5a7c5f5b4c28e82ef9ebd6b8d41bf53d10b0aee9
Max <max@foodia.org>
2014-10-28 09:18:48 -0700

    .

--- app/controllers/api/v3/reports_controller.rb
+++ app/controllers/api/v3/reports_controller.rb
@@ -5,8 +5,14 @@ class Api::V3::ReportsController < Api::V3::ApiController
     def index
         project = Project.find params[:project_id]
         if project.reports 
+            if project.id == 37
+                puts "using experimental thing"
+                reports = project.reports.order('report_date DESC')
+            else
+                reports = project.reports.sort_by(&:date_for_sort).reverse
+            end
             respond_to do |format|
-                format.json { render_for_api :v3_reports, :json => project.reports.sort_by(&:date_for_sort).reverse, :root => :reports}
+                format.json { render_for_api :v3_reports, :json => reports, :root => :reports}
             end
         else
             render :json => {:success => false}
