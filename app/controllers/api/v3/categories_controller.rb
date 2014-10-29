f63214c8c3fe9e17dd67ef85407b399179f2989b
Max <max@foodia.org>
2014-10-29 10:24:17 -0700

    .

--- app/controllers/api/v3/categories_controller.rb
+++ app/controllers/api/v3/categories_controller.rb
@@ -3,7 +3,7 @@ class Api::V3::CategoriesController < Api::V3::ApiController
     def show
     	category = Category.find params[:id]
     	respond_to do |format|
-        	format.json { render_for_api :checklists, :json => category, :root => :category}
+        	format.json { render_for_api :v3_checklists, :json => category, :root => :category}
       	end
     end
 
--- app/controllers/api/v3/phases_controller.rb
+++ app/controllers/api/v3/phases_controller.rb
@@ -3,7 +3,7 @@ class Api::V3::PhasesController < Api::V3::ApiController
     def show
     	phase = Phase.find params[:id]
     	respond_to do |format|
-        	format.json { render_for_api :checklists, :json => phase, :root => :phase}
+        	format.json { render_for_api :v3_checklists, :json => phase, :root => :phase}
       	end
     end
 
