cf2a0ac5f31a2ccfea6b1b5104d00707a26d9622
Max <max@foodia.org>
2014-10-30 16:30:07 -0700

    .

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587,
     :user_name => ENV['MANDRILL_USERNAME'],
-    :password  => ENV['MANDRILL_PASSWORD'],
-    :domain    => 'heroku.com',
+    :password  => ENV['MANDRILL_APIKEY'],
+    :domain    => 'buildhawk.com',
     :authentication => :plain
   }
 ActionMailer::Base.delivery_method = :smtp
