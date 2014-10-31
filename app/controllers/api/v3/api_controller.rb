ca5f6c8035b9adf8042228c3740a331f8f0556b1
Max <max@foodia.org>
2014-10-30 17:16:56 -0700

    .

--- app/controllers/api/v3/api_controller.rb
+++ app/controllers/api/v3/api_controller.rb
@@ -1,12 +1,12 @@
 class Api::V3::ApiController < ApplicationController
-    http_basic_authenticate_with name: "buildhawk_mobile", password: "aca344dc4b27b82f994094d8c9bab0af"#, except: :index
+    #http_basic_authenticate_with name: "buildhawk_mobile", password: "aca344dc4b27b82f994094d8c9bab0af"#, except: :index
   	skip_before_filter :verify_authenticity_token
   	before_filter :verify_mobile_token
 
   	private
   	
   	def verify_mobile_token
-        puts "Request: #{request.env['HTTP_DEVICE_TYPE']}"
+        #puts "Request: #{request.env['HTTP_DEVICE_TYPE']}"
   		if params[:device_type].present? && params[:mobile_token].present? 
             @user = User.where(mobile_token: params[:mobile_token]).first
   			if @user
