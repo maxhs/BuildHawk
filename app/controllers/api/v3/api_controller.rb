47ca73af23e9d533558be491bcd177bb66bc359b
Max <max@foodia.org>
2014-11-08 08:58:01 -0800

    .

--- app/controllers/api/v3/api_controller.rb
+++ app/controllers/api/v3/api_controller.rb
@@ -6,7 +6,6 @@ class Api::V3::ApiController < ApplicationController
   	private
   	
   	def verify_mobile_token
-        #puts "Request: #{request.env['HTTP_DEVICE_TYPE']}"
         @device_type = request.env['HTTP_DEVICE_TYPE'].to_i
   		if params[:device_type].present? && params[:mobile_token].present? 
             @user = User.where(mobile_token: params[:mobile_token]).first
