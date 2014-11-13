78c57e7b109d9e06a2cf353e3979362fd450d146
Max <max@foodia.org>
2014-11-12 16:22:20 -0800

    .

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,19 +2,19 @@ class ServicesController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 
 	def support
-		
+		render json: {success: true}
 	end
 
 	def tasks
-		
+		render json: {success: true}
 	end
 
 	def reports
-		
+		render json: {success: true}
 	end
 
 	def checklists
-		
+		render json: {success: true}
 	end
 
 end
