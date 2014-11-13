545dafcb8599a3bf30fe82a6c4d75e96bf5cba47
Max <max@foodia.org>
2014-11-12 16:11:57 -0800

    .

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,20 +1,20 @@
 class ServicesController < ApplicationController
 	skip_before_filter :verify_authenticity_token
-	
+
 	def support
-		puts "support webhook from mandrill: #{parameters}"
+		
 	end
 
 	def tasks
-		puts "tasks webhooks from mandrill: #{parameters}"
+		
 	end
 
 	def reports
-		puts "reports webhooks from mandrill: #{parameters}"
+		
 	end
 
 	def checklists
-		puts "checklists webhooks from mandrill: #{parameters}"
+		
 	end
 
 end
