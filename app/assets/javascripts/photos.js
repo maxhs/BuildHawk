4ca95429e7fda51fe7ad4f0259d3ad2e4f74a2cc
Max <max@foodia.org>
2014-11-21 14:56:59 -0800

    .

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -6,3 +6,20 @@ function documentPhotos(){
 	    Shadowbox.setup(".shadow-photo", {});
 	});
 }
+
+function largeImage(){
+	Shadowbox.clearCache();
+	Shadowbox.setup(".shadow-photo", {});
+	$("#photo_name,#photo_description").focus(function(){
+		$('#click-to-edit').fadeOut(170,function(){
+			$(this).text('(enter/return to save)');
+			$(this).fadeIn(170);
+		});
+	});
+	$("#photo_name,#photo_description").focusout(function(){
+		$('#click-to-edit').fadeOut(170,function(){
+			$(this).text('(click name to edit)');
+			$(this).fadeIn(170);
+		});
+	});
+}
