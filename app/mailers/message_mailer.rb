dc35ed95c418d96709073a044ccc93e4740d436b
Max <max@foodia.org>
2014-11-21 17:04:54 -0800

    .

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,13 +1,15 @@
 class MessageMailer < ActionMailer::Base
   	layout "standard_mailer"
+  	include ApplicationHelper
 
   	def send_message(message, user)
   		@message = message
   		@user = user
+  		@timestamp = parse_datetime(message.created_at)
   		mail(
       		:subject => "#{message.body[0..15]}...",
       		:to      => user.email,
-      		:from 	 => "support@buildhawk.com",
+      		:from 	 => "BuildHawk Message <support@buildhawk.com>",
       		:tag     => 'Message'
     	)
   	end
