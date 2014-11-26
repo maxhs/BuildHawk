5e1cb5a8b2b96b4235946d69fb7ee515bb8bc67c
Max <max@foodia.org>
2014-11-25 16:22:37 -0800

    .

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -111,9 +111,9 @@ class Checklist < ActiveRecord::Base
 
     	def open_spreadsheet(file)
             case File.extname(file.original_filename)
-            when ".csv" then Csv.new(file.path, nil, :ignore)
-            when ".xls" then Excel.new(file.path, nil, :ignore)
-            when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+            when ".csv" then Roo::Csv.new(file.path)
+            when ".xls" then Roo::Excel.new(file.path)
+            when ".xlsx" then Roo::Excelx.new(file.path)
             else raise "Unknown file type: #{file.original_filename}"
             end
     	end
