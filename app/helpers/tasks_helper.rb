2f07fb8de860ca88555e9e0d08b93588bce97e46
Max <max@foodia.org>
2014-11-25 16:19:01 -0800

    .

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,10 +2,12 @@ module TasksHelper
 	def cleanText(text)
 		cleaned = text.partition('On Mon').first.html_safe
 		cleaned = text.partition('On Tue').first.html_safe
+		puts "what is cleaned now? #{cleaned}"
 		cleaned = text.partition('On Wed').first.html_safe
 		cleaned = text.partition('On Thu').first.html_safe
 		cleaned = text.partition('On Fri').first.html_safe
 		cleaned = text.partition('On Sat').first.html_safe
 		cleaned = text.partition('On Sun').first.html_safe
+		return cleaned
 	end
 end
